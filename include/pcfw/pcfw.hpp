c10655e2fadf0786c22ad7d74bda42a0d3bd2f05
Nauta <oknauta@proton.me>
2024-12-09 20:18:51 -0300

    Added support for dllexport and dllimport

--- include/pcfw/pcfw.hpp
+++ include/pcfw/pcfw.hpp
@@ -6,22 +6,33 @@
 #ifndef _PCFW_HPP
 #define _PCFW_HPP
 
+#ifdef _WIN32
+#ifdef PCFW_EXPORTS
+#define PCFW_API __declspec(dllexport)
+#else
+#define PCFW_API __declspec(dllimport)
+#endif
+#else
+#define PCFW_API
+#endif
+
+
 namespace PCFW
 {
     typedef void (*framebuffer_size_callback)(int width, int height);
     typedef struct window window;
 
-    window *createWindow(int width, int height, const char *title);
-    int getWindowWidth(window *window);
-    int getWindowHeight(window *window);
-    void destroyWindow(window *window);
-    void swapBuffers(window *window);
-    void makeCurrentContext(window *window);
-    void pollEvents(window *window);
-    void setFramebufferSizeCallback(window *window, framebuffer_size_callback callback);
-    void setSwapInterval(window *window, int interval);
-    int windowShouldClose(window *window);
-    const char *getWindowTitle(window *window);
+    PCFW_API window *createWindow(int width, int height, const char *title);
+    PCFW_API int getWindowWidth(window *window);
+    PCFW_API int getWindowHeight(window *window);
+    PCFW_API void destroyWindow(window *window);
+    PCFW_API void swapBuffers(window *window);
+    PCFW_API void makeCurrentContext(window *window);
+    PCFW_API void pollEvents(window *window);
+    PCFW_API void setFramebufferSizeCallback(window *window, framebuffer_size_callback callback);
+    PCFW_API void setSwapInterval(window *window, int interval);
+    PCFW_API int windowShouldClose(window *window);
+    PCFW_API const char *getWindowTitle(window *window);
 
 } // namespace PCFW
 
